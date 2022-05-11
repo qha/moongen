94c6ca2b690525928f1640e4bd4554daff0602ca
Ulrik Haugen <ulrik.haugen@liu.se>
2022-05-11 16:34:49 +0200

    Add replay-pcap-multi.lua to examples/pcap

---
+++ examples/pcap/replay-pcap-multi.lua
@@ -0,0 +1,152 @@
+--- Replay pcap files on multiple interfaces.
+--
+-- Please note that if your storage system bandwidth does not match or
+-- exceed the combined bandwidth of the interfaces you want to replay
+-- packets on make sure that your pcaps fit in memory.
+--
+-- Also note that you will want pcaps captured on named interfaces so
+-- that they contain link layer headers rather than pcaps captured on
+-- "any" interface with fudged link layer headers.
+
+local table = require "table"
+
+local mg = require "moongen"
+local device = require "device"
+local memory = require "memory"
+local stats = require "stats"
+local log = require "log"
+local pcap = require "pcap"
+local limiter = require "software-ratecontrol"
+
+
+function configure(parser)
+   parser:option("--dev", "Device to use.")
+      :args(1)
+      :count("1+")
+      :convert(tonumber)
+      :target("devs")
+   parser:argument("files", "Files to replay, must be supplied"
+                      .. " as many times as --dev.")
+      :args("1+")
+   parser:option("-r --rate-multiplier",
+                 "Speed up or slow down replay,"
+                    .. " 1 = use intervals from files,"
+                    .. " default = replay as fast as possible.")
+      :default(0)
+      :convert(tonumber)
+      :target("rateMultiplier")
+   parser:option("-s --buffer-flush-time",
+                 "Time to wait before stopping Moongen after enqueuing"
+                    .. " all packets. Increase for pcaps with a very low"
+                    .. " rate.")
+      :default(10)
+      :convert(tonumber)
+      :target("bufferFlushTime")
+   parser:flag("-l --loop", "Repeat pcap files until interrupted.")
+   parser:option("-n --repetitions",
+                 "Repeat pcap files this number of times")
+      :default(1)
+      :convert(tonumber)
+   local args = parser:parse()
+   return args
+end
+
+function master(args)
+   local devs = {}
+   for ii, dev in ipairs(args.devs) do
+      table.insert(devs, device.config({port = dev}))
+   end
+   device.waitForLinks()
+   local rateLimiters = {}
+   if args.rateMultiplier > 0 then
+      for ii, dev in ipairs(devs) do
+         table.insert(rateLimiters, limiter:new(dev:getTxQueue(0), "custom"))
+      end
+   end
+   local replayers = {}
+   for ii, dev in ipairs(devs) do
+      table.insert(replayers,
+                   mg.startTask("replay",
+                                dev:getTxQueue(0),
+                                args.files[ii],
+                                args.loop,
+                                args.repetitions,
+                                rateLimiters[ii],
+                                args.rateMultiplier,
+                                args.bufferFlushTime))
+      stats.startStatsTask{txDevices = {dev}}
+   end
+   for ii, replayer in ipairs(replayers) do
+      replayer:wait()
+   end
+   mg:stop()
+   mg.waitForTasks()
+end
+
+function replay(queue,
+                file,
+                loop,
+                repetitions,
+                rateLimiter,
+                multiplier,
+                sleepTime)
+   local mempool = memory:createMemPool(4096)
+   local bufs = mempool:bufArray()
+   local pcapFile = pcap:newReader(file)
+   local prev = 0
+   local transmissions = 0
+   local linkSpeed = queue.dev:getLinkStatus().speed
+   log:info("Replaying %s on %s", file, queue.dev)
+   log:info("Link speed %s for %s", linkSpeed, queue.dev)
+
+   while mg.running() do
+      local n = pcapFile:read(bufs)
+      if n > 0 then
+         if rateLimiter ~= nil then
+            if prev == 0 then
+               prev = bufs.array[0].udata64
+            end
+            for i = 1, n do
+               local buf = bufs[i]
+               -- ts is in microseconds
+               local ts = buf.udata64
+               if prev > ts then
+                  ts = prev
+               end
+               local delay = ts - prev
+               delay = tonumber(delay * 10^3) / multiplier -- nanoseconds
+               delay = delay / (8000 / linkSpeed) -- delay in bytes
+               buf:setDelay(delay)
+               prev = ts
+            end
+         end
+      else
+         transmissions = transmissions + 1
+         if loop then
+            prev = 0
+            pcapFile:reset()
+            log:info("%s exhausted, starting %d retransmission on %s",
+                     file, transmissions, queue.dev)
+         elseif transmissions < repetitions then
+            prev = 0
+            pcapFile:reset()
+            log:info("%s exhausted, starting %d/%d retransmission on %s",
+                     file, transmissions, repetitions, queue.dev)
+         else
+            break
+         end
+      end
+      if rateLimiter then
+         rateLimiter:sendN(bufs, n)
+      else
+         queue:sendN(bufs, n)
+      end
+   end
+
+   log:info("Replay on %s: Enqueued all packets,"
+            .. " waiting %d seconds for queues to flush",
+            queue.dev,
+            sleepTime)
+   mg.sleepMillisIdle(sleepTime * 1000)
+   pcapFile:close()
+end
